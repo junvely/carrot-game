985e7c03d656b68a291e28e59c0c04cceb422950
junvely <junvely97@gmail.com>
2022-05-27 01:35:50 +0900

    Delete item target when clicked
    - Click to delete if target is carrot and count carrot
    - Click to end the game if the target is a bug

---
+++ js/delete_items.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const carrotSound = new Audio("sound/carrot_pull.mp3");
+const bugSound = new Audio("sound/bug_pull.mp3");
+
+items.addEventListener("click", (e) => {
+  const dataset = e.target.dataset;
+  const id = dataset.id;
+  const type = dataset.type;
+  const idTarget = document.querySelector(`.item[data-id="${id}"`);
+
+  if (id && type === "carrot") {
+    carrotSound.play();
+    idTarget.remove();
+    countCarrot();
+  } else if (id && type === "bug") {
+    bugSound.play();
+    endGame();
+  }
+});
