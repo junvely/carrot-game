c58d1d6e6ae2bb69316397ade232283448755214
junvely <junvely97@gmail.com>
2022-05-27 23:42:24 +0900

    Update Variables
    - add count variable

--- js/create_carrots.js
+++ js/create_carrots.js
@@ -3,10 +3,11 @@
 const items = document.querySelector(".items");
 const carrots = [];
 const bugs = [];
+const count = 10;
 let id = 0;
 
 function arrPushItems() {
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j < count; j++) {
     carrots.push(
       `<li class="item carrot" data-id="${id}" data-type="carrot"></li>`
     );
@@ -18,5 +19,5 @@ function arrPushItems() {
 function innerItems() {
   arrPushItems();
   items.innerHTML = `${carrots.join("")}${bugs.join("")}`;
-  itemRandomCoord();
+  randomCoord();
 }
